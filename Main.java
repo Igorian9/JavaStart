7a5815b00d6e47a12b85b6cc73b076b44738ecfc
Igorian9 <32694618+Igorian9@users.noreply.github.com>
2017-10-25 21:59:40 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package ua.i.igor_igorovuich;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
